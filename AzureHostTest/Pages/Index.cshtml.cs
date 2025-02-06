7bf9cc8fc62bfc970cdec1d8df6a18f2cd53db31
aslavuta <aslavuta@gmail.com>
2025-02-06 17:35:25 +0200

    Configuration test

--- AzureHostTest/Pages/Index.cshtml.cs
+++ AzureHostTest/Pages/Index.cshtml.cs
@@ -6,15 +6,25 @@ namespace AzureHostTest.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly IConfiguration Configuration;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
         {
+            Configuration = configuration;
             _logger = logger;
         }
 
         public void OnGet()
         {
 
+        }
+
+        public string Message { get; set; }
+
+        public void OnPostReadApplicationProperty()
+        {
+            var appSettingValue = Configuration["ApplicationSetting1"];
+            Message = $"Application Settings value is: {appSettingValue}";
         }
     }
 }
